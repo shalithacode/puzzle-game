17b6b04cce67086b8340a11a5e6d7aabf82b633c
shalithacode <shalithalakruwan1998@gmail.com>
2023-03-16 11:15:24 +0530

    edited

--- index.js
+++ index.js
@@ -7,78 +7,6 @@ let draggedEndCell = null;
 let img1 = null;
 let img2 = null;
 cells.forEach((cell, i) => {
-  cell.addEventListener("touchstart", () => {
-    draggedCell = cell;
-    setTimeout(() => {
-      cell.classList.add("dragging");
-    }, 0);
-  });
-
-  cell.addEventListener("touchend", () => {
-    draggedEndCell = cell.id;
-    if (img1.outerHTML.split(".")[0].slice(-1) !== draggedEndCell.slice(-1)) {
-      draggedCell.appendChild(img1);
-      cell.appendChild(img2);
-      document.getElementById("msg").innerHTML = `Wrong move, Try again!`;
-      setTimeout(() => {
-        document.getElementById("msg").innerText = "";
-      }, 2500);
-    }
-    draggedCell = null;
-    cell.classList.remove("dragging");
-  });
-
-  cell.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
-
-  cell.addEventListener("touchmove", (e) => {
-    e.preventDefault();
-    cell.classList.add("hovered");
-  });
-
-  cell.addEventListener("touchleave", () => {
-    cell.classList.remove("hovered");
-  });
-
-  cell.addEventListener("touchend", () => {
-    tries++;
-    if (draggedCell !== null) {
-      img1 = draggedCell.querySelector("img");
-      img2 = cell.querySelector("img");
-
-      if (img1 !== img2) {
-        draggedCell.appendChild(img2);
-        cell.appendChild(img1);
-      } else {
-        alert("Can't swap");
-      }
-    } else {
-      alert("Can't drop here!");
-    }
-
-    let count = 0;
-    document.querySelectorAll(".cell").forEach((cell, i) => {
-      // console.log(`imgX3/${i + 1}.png`);
-      // console.log(cell.querySelector("img").getAttribute("src"));
-      if (
-        cell.querySelector("img").getAttribute("src") ===
-        `imgX${mode}/${i + 1}.png`
-      ) {
-        count++;
-        // console.log("hi");
-      }
-    });
-    if (count === mode * mode) {
-      document.getElementById(
-        "msg"
-      ).innerText = `You have won in ${tries} tries`;
-      tryCount = 0;
-      console.log(`pass in ${tries}`);
-    }
-  });
-
-  ////////////////////////////////////////
   cell.addEventListener("dragstart", () => {
     draggedCell = cell;
     setTimeout(() => {
@@ -150,6 +78,79 @@ cells.forEach((cell, i) => {
     }
   });
 });
+cells.forEach((cell, i) => {
+  cell.addEventListener("touchstart", (e) => {
+    draggedCell = cell;
+    setTimeout(() => {
+      cell.classList.add("dragging");
+    }, 0);
+  });
+
+  cell.addEventListener("touchend", (e) => {
+    draggedEndCell = cell.id;
+    if (img1.outerHTML.split(".")[0].slice(-1) !== draggedEndCell.slice(-1)) {
+      draggedCell.appendChild(img1);
+      cell.appendChild(img2);
+      document.getElementById("msg").innerHTML = `Wrong move, Try again!`;
+      setTimeout(() => {
+        document.getElementById("msg").innerText = "";
+      }, 2500);
+    }
+    draggedCell = null;
+    cell.classList.remove("dragging");
+  });
+
+  cell.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+  });
+
+  cell.addEventListener("touchenter", (e) => {
+    e.preventDefault();
+    cell.classList.add("hovered");
+  });
+
+  cell.addEventListener("touchleave", () => {
+    cell.classList.remove("hovered");
+  });
+
+  cell.addEventListener("touchend", () => {
+    tries++;
+    if (draggedCell !== null) {
+      img1 = draggedCell.querySelector("img");
+      img2 = cell.querySelector("img");
+
+      if (img1 !== img2) {
+        draggedCell.appendChild(img2);
+        cell.appendChild(img1);
+      } else {
+        alert("Can't swap");
+      }
+    } else {
+      alert("Can't drop here!");
+    }
+
+    let count = 0;
+    document.querySelectorAll(".cell").forEach((cell, i) => {
+      // console.log(`imgX3/${i + 1}.png`);
+      // console.log(cell.querySelector("img").getAttribute("src"));
+      if (
+        cell.querySelector("img").getAttribute("src") ===
+        `imgX${mode}/${i + 1}.png`
+      ) {
+        count++;
+        // console.log("hi");
+      }
+    });
+    if (count === mode * mode) {
+      document.getElementById(
+        "msg"
+      ).innerText = `You have won in ${tries} tries`;
+      tryCount = 0;
+      console.log(`pass in ${tries}`);
+    }
+  });
+});
+
 function shuffleImages() {
   document.getElementById("msg").innerText = "";
   const cells = document.querySelectorAll(".cell");
