1931846ddb0336c21f6d98a723eecf8147ca0695
shalithacode <shalithalakruwan1998@gmail.com>
2023-03-17 05:49:24 +0530

    updated

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ let draggedEndCell = null;
 
 let img1 = null;
 let img2 = null;
+
+let activeCell = null;
+let swapCell = null;
+let startX = 0;
+let startY = 0;
+
 cells.forEach((cell, i) => {
+  //tough events
+  cell.addEventListener("touchstart", handleTouchStart);
+  cell.addEventListener("touchmove", handleTouchMove);
+  cell.addEventListener("touchend", handleTouchEnd);
+
   cell.addEventListener("dragstart", () => {
     draggedCell = cell;
     setTimeout(() => {
@@ -41,6 +52,23 @@ cells.forEach((cell, i) => {
     cell.classList.remove("hovered");
   });
 
+  function isWinner() {
+    let count = 0;
+    document.querySelectorAll(".cell").forEach((cell, i) => {
+      if (
+        cell.querySelector("img").getAttribute("src") ===
+        `imgX${mode}/${i + 1}.png`
+      ) {
+        count++;
+      }
+    });
+    if (count === mode * mode && tries > 1) {
+      document.getElementById(
+        "msg"
+      ).innerText = `You have won in ${tries} tries`;
+      tries = 0;
+    }
+  }
   cell.addEventListener("drop", () => {
     tries++;
     if (draggedCell !== null) {
@@ -57,98 +85,73 @@ cells.forEach((cell, i) => {
       alert("Can't drop here!");
     }
 
-    let count = 0;
-    document.querySelectorAll(".cell").forEach((cell, i) => {
-      // console.log(`imgX3/${i + 1}.png`);
-      // console.log(cell.querySelector("img").getAttribute("src"));
+    isWinner();
+  });
+
+  function handleTouchEnd(event) {
+    if (!activeCell) return;
+    activeCell.classList.add("active");
+    if (swapCell) {
+      const activeImg = activeCell.querySelector("img");
+      const swapImg = swapCell.querySelector("img");
 
       if (
-        cell.querySelector("img").getAttribute("src") ===
-        `imgX${mode}/${i + 1}.png`
+        `imgX${mode}/${swapCell.getAttribute("id").replace("cell", "")}.png` ===
+        activeCell.querySelector("img").getAttribute("src")
       ) {
-        count++;
-        // console.log("hi");
-      }
-    });
-    if (count === mode * mode) {
-      document.getElementById(
-        "msg"
-      ).innerText = `You have won in ${tries} tries`;
-      tryCount = 0;
-      console.log(`pass in ${tries}`);
-    }
-  });
-});
-cells.forEach((cell, i) => {
-  cell.addEventListener("touchstart", (e) => {
-    console.log("hiii");
-    draggedCell = cell;
-    setTimeout(() => {
-      cell.classList.add("dragging");
-    }, 0);
-  });
-
-  cell.addEventListener("touchend", (e) => {
-    draggedEndCell = cell.id;
-    // if (img1.outerHTML.split(".")[0].slice(-1) !== draggedEndCell.slice(-1)) {
-    //   draggedCell.appendChild(img1);
-    //   cell.appendChild(img2);
-    //   document.getElementById("msg").innerHTML = `Wrong move, Try again!`;
-    //   setTimeout(() => {
-    //     document.getElementById("msg").innerText = "";
-    //   }, 2500);
-    // }
-    draggedCell = null;
-    cell.classList.remove("dragging");
-  });
-
-  cell.addEventListener("touchmove", (e) => {
-    e.preventDefault();
-  });
-
-  cell.addEventListener("touchenter", (e) => {
-    e.preventDefault();
-    cell.classList.add("hovered");
-  });
-
-  cell.addEventListener("touchleave", () => {
-    cell.classList.remove("hovered");
-  });
-
-  cell.addEventListener("touchend", () => {
-    tries++;
-    if (draggedCell !== null) {
-      img1 = draggedCell.querySelector("img");
-      img2 = cell.querySelector("img");
-
-      if (img1 !== img2) {
-        draggedCell.appendChild(img2);
-        cell.appendChild(img1);
+        activeCell.appendChild(swapImg);
+        swapCell.appendChild(activeImg);
       } else {
-        alert("Can't swap");
+        document.getElementById("msg").innerHTML = `Wrong move, Try again!`;
+        setTimeout(() => {
+          document.getElementById("msg").innerText = "";
+        }, 2500);
       }
-    } else {
-      alert("Can't drop here!");
     }
-
-    // let count = 0;
-    // document.querySelectorAll(".cell").forEach((cell, i) => {
-    //   // console.log(`imgX3/${i + 1}.png`);
-    //   // console.log(cell.querySelector("img").getAttribute("src"));
-    //   const img = cell.querySelector("img");
-    //   if (img && img.getAttribute("src") === `imgX${mode}/${i + 1}.png`) {
-    //     count++;
-    //   }
-    // });
-    // if (count === mode * mode) {
-    //   document.getElementById(
-    //     "msg"
-    //   ).innerText = `You have won in ${tries} tries`;
-    //   tryCount = 0;
-    //   console.log(`pass in ${tries}`);
-    // }
-  });
+    activeCell = null;
+    swapCell = null;
+    setTimeout(() => {
+      cells.forEach((cell) => (cell.style.transform = ""));
+      cells.forEach((cell) => cell.classList.remove("active", "swap"));
+      setTimeout(() => {
+        cells.forEach((cell) => (cell.querySelector("img").style.opacity = 1));
+      }, 300);
+    }, 300);
+    isWinner();
+  }
 });
+
+function handleTouchStart(event) {
+  tries++;
+  activeCell = event.target.closest(".cell");
+  startX = event.touches[0].clientX;
+  startY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+  if (!activeCell) return;
+  event.preventDefault();
+  const currentX = event.touches[0].clientX;
+  const currentY = event.touches[0].clientY;
+  const diffX = currentX - startX;
+  const diffY = currentY - startY;
+  activeCell.style.transform = `translate(${diffX}px, ${diffY}px)`;
+  cells.forEach((cell) => {
+    if (cell === activeCell) return;
+    const rect = cell.getBoundingClientRect();
+    if (
+      currentX > rect.left &&
+      currentX < rect.right &&
+      currentY > rect.top &&
+      currentY < rect.bottom
+    ) {
+      swapCell = cell;
+      swapCell.classList.add("swap");
+    } else {
+      cell.classList.remove("swap");
+    }
+  });
+}
 
 function shuffleImages() {
   document.getElementById("msg").innerText = "";
